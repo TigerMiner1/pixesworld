5f0f3561cc2db24e0c5d638f1fb1b4eab2252583
LEBON <lebon1377@gmail.com>
2019-02-15 09:33:17 +0530

    yas

--- bot.js
+++ bot.js
@@ -306,7 +306,7 @@ client.on("message", async function (message) {
         .setAuthor(`Weather for ${current.observationpoint}`) // This shows the current location of the weather.
         .setThumbnail(current.imageUrl) // This sets the thumbnail of the embed
         .setColor(0x00AE86) // This sets the color of the embed, you can set this to anything if you look put a hex color picker, just make sure you put 0x infront of the hex
-        .addField('Timezone', `UTC${location.timezone}`, true) // This is the first field, it shows the timezone, and the true means `inline`, you can read more about this on the official discord.js documentation
+        .addField('Timezone', `IST${location.timezone}`, true) // This is the first field, it shows the timezone, and the true means `inline`, you can read more about this on the official discord.js documentation
         .addField('Degree Type', location.degreetype, true)// This is the field that shows the degree type, and is inline
         .addField('Temperature', `${current.temperature} Degrees`, true)
         .addField('Feels Like', `${current.feelslike} Degrees`, true)
@@ -368,7 +368,7 @@ client.on("message", async function (message) {
 
   if (command === "ping") {
       const msg = await message.channel.send("Pinging...");
-      await msg.edit(`Pong! (Took: ${msg.createdTimestamp - message.createdTimestamp}ms.)`);
+      await msg.edit(`:robot: Bot Ping: (Took: ${msg.createdTimestamp - message.createdTimestamp}ms.)`);
   }
 
   if (command === "shutdown" && botConfigs.plugins[10].activated == true) {
